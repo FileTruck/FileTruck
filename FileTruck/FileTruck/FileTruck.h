3c660ba83b761ed0b1aba9c80c8189098c74d3c7
Chris Grant <cgrant@scottlogic.co.uk>
2014-03-06 14:05:14 +0000

    Adding starter project.

---
+++ FileTruck/FileTruck/FileTruck.h
@@ -0,0 +1,13 @@
+//
+//  FileTruck.h
+//  FileTruck
+//
+//  Created by Chris Grant on 06/03/2014.
+//  Copyright (c) 2014 ScottLogic. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface FileTruck : NSObject
+
+@end
